29f37dcaca577f610d540ed5b0fef17c9f31178d
Hugo Costa <hcosta@silicolife.com>
2016-04-21 17:09:54 +0100

    [update] Corpus Creation acoording to new API

--- src/main/java/com/silicolife/textmining/processes/corpora/loaders/CorpusCreation.java
+++ src/main/java/com/silicolife/textmining/processes/corpora/loaders/CorpusCreation.java
@@ -3,7 +3,6 @@ package com.silicolife.textmining.processes.corpora.loaders;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
@@ -24,89 +23,108 @@ import com.silicolife.textmining.core.interfaces.core.document.corpus.ICorpus;
 import com.silicolife.textmining.core.interfaces.core.report.corpora.ICorpusCreateReport;
 
 public class CorpusCreation {
-	
+
 	public CorpusCreation()
 	{
-		
+
 	}
-	
+
 
 	public ICorpusCreateReport createCorpus(ICorpusCreateConfiguration configuration) throws ANoteException
 	{
 		try {
-		Properties properties = configuration.getProperties();
-		properties.put(GlobalNames.textType, CorpusTextType.convertCorpusTetTypeToString(configuration.getCorpusTextType()));
-		ICorpus newCorpus = new CorpusImpl(configuration.getCorpusName(), configuration.getCorpusNotes(), configuration.getProperties());
-		InitConfiguration.getDataAccess().createCorpus(newCorpus);
-		Set<IPublication> documents = configuration.getDocuments();
-		long startTime = GregorianCalendar.getInstance().getTimeInMillis();
-		int step = 0;
-		int total = documents.size();
-		for(IPublication publication:documents)
-		{
-			// Before connect ... insert document if not exists
-			if(InitConfiguration.getDataAccess().getPublication(publication.getId())==null)
-			{		
-				List<IPublication> documentToadd = new ArrayList<IPublication>();
-				documentToadd.add(publication);
-				// Add Publication to system
-				InitConfiguration.getDataAccess().addPublications(documentToadd );
-				// Test if Corpus is full text
-				if(configuration.getCorpusTextType().equals(CorpusTextType.Hybrid) || 
-						configuration.getCorpusTextType().equals(CorpusTextType.FullText))
-				{
-					// IF PDF is nt available and source URL is a file put file in directory and update Full text COntent
-					if(publication.getSourceURL()!=null)
-					{
-						publication.addPDFFile(new File(publication.getSourceURL()));
-						// update relative path
-						InitConfiguration.getDataAccess().updatePublication(publication);
-
-					}
-					// update full text context
-					if(publication.getFullTextContent().isEmpty())
-						InitConfiguration.getDataAccess().updatePublicationFullTextContent(publication);
-				}
-				
-			}
-			else // Already Exist publication
+			Properties properties = configuration.getProperties();
+			properties.put(GlobalNames.textType, CorpusTextType.convertCorpusTetTypeToString(configuration.getCorpusTextType()));
+			ICorpus newCorpus = new CorpusImpl(configuration.getCorpusName(), configuration.getCorpusNotes(), configuration.getProperties());
+			createCorpusOnDatabase(newCorpus);
+			Set<IPublication> documents = configuration.getDocuments();
+			int step = 0;
+			int total = documents.size();
+			for(IPublication publication:documents)
 			{
-				if(configuration.getCorpusTextType().equals(CorpusTextType.Hybrid) || 
-						configuration.getCorpusTextType().equals(CorpusTextType.FullText))
-				{
-					// IF PDF is not available and source URL is a file put file in directory and update Full text COntent
-					if(publication.getSourceURL()!=null && !publication.isPDFAvailable())
+				// Before connect ... insert document if not exists
+				IPublication pub = getPublicationOnDatabaseByID(publication.getId());
+				if(pub==null)
+				{		
+					List<IPublication> documentToadd = new ArrayList<IPublication>();
+					documentToadd.add(publication);
+					// Add Publication to system
+					addPublicationToDatabase(documentToadd);
+					// Test if Corpus is full text
+					if(configuration.getCorpusTextType().equals(CorpusTextType.Hybrid) || 
+							configuration.getCorpusTextType().equals(CorpusTextType.FullText))
 					{
-						publication.addPDFFile(new File(publication.getSourceURL()));
-						// update relative path
-						InitConfiguration.getDataAccess().updatePublication(publication);
-					}
-					// PDF is availbale and Full text are not available yet
-					else if(publication.isPDFAvailable() && publication.getFullTextContent().isEmpty())
-					{
-						String saveDocDirectoty = (String) PropertiesManager.getPManager().getProperty(GeneralDefaultSettings.PDFDOCDIRECTORY);
-						// Get PDF to text from PDF file
-						String fullTextContent = PDFtoText.convertPDFDocument(saveDocDirectoty + "//" + publication.getRelativePath());
-						publication.setFullTextContent(fullTextContent);
+						// IF PDF is nt available and source URL is a file put file in directory and update Full text COntent
+						if(publication.getSourceURL()!=null)
+						{
+							publication.addPDFFile(new File(publication.getSourceURL()));
+							// update relative path
+							InitConfiguration.getDataAccess().updatePublication(publication);
+
+						}
 						// update full text context
-						InitConfiguration.getDataAccess().updatePublicationFullTextContent(publication);
+						if(publication.getFullTextContent().isEmpty())
+							updatePublicationFullTextOnfDatabase(publication);
+					}
+
+				}
+				else // Already Exist publication
+				{
+					if(configuration.getCorpusTextType().equals(CorpusTextType.Hybrid) || 
+							configuration.getCorpusTextType().equals(CorpusTextType.FullText))
+					{
+						// IF PDF is not available and source URL is a file put file in directory and update Full text COntent
+						if(publication.getSourceURL()!=null && !publication.isPDFAvailable())
+						{
+							publication.addPDFFile(new File(publication.getSourceURL()));
+							// update relative path
+							InitConfiguration.getDataAccess().updatePublication(publication);
+						}
+						// PDF is availbale and Full text are not available yet
+						else if(publication.isPDFAvailable() && publication.getFullTextContent().isEmpty())
+						{
+							String saveDocDirectoty = (String) PropertiesManager.getPManager().getProperty(GeneralDefaultSettings.PDFDOCDIRECTORY);
+							// Get PDF to text from PDF file
+							String fullTextContent = PDFtoText.convertPDFDocument(saveDocDirectoty + "//" + publication.getRelativePath());
+							publication.setFullTextContent(fullTextContent);
+							updatePublicationFullTextOnfDatabase(publication);
+						}
 					}
 				}
+
+				InitConfiguration.getDataAccess().addCorpusPublication(newCorpus, publication);
+				step++;
+				if(step%10==0)
+					memoryAndProgress(step,total);
 			}
-			
-			InitConfiguration.getDataAccess().addCorpusPublication(newCorpus, publication);
-			step++;
-			if(step%10==0)
-				memoryAndProgressAndTime(step,total,startTime);
-		}
-		ICorpusCreateReport report = new CorpusCreateReportImpl(newCorpus, configuration.getCorpusTextType(),configuration.getDocuments().size());
-		return report;
+			ICorpusCreateReport report = new CorpusCreateReportImpl(newCorpus, configuration.getCorpusTextType(),configuration.getDocuments().size());
+			return report;
 		} catch (IOException e) {
 			throw new ANoteException(e);
 		}
 	}
-	
-	protected void memoryAndProgressAndTime(int step, int total,long startTime) {
+
+
+	protected void updatePublicationFullTextOnfDatabase(IPublication publication) throws ANoteException {
+		InitConfiguration.getDataAccess().updatePublicationFullTextContent(publication);
+	}
+
+
+	protected void addPublicationToDatabase(List<IPublication> documentToadd)throws ANoteException {
+		InitConfiguration.getDataAccess().addPublications(documentToadd );
+	}
+
+
+	protected IPublication getPublicationOnDatabaseByID(Long publictionID)throws ANoteException {
+		return InitConfiguration.getDataAccess().getPublication(publictionID);
+	}
+
+
+	protected void createCorpusOnDatabase(ICorpus newCorpus)throws ANoteException {
+		InitConfiguration.getDataAccess().createCorpus(newCorpus);
+	}
+
+	protected void memoryAndProgress(int step, int total) {
 		System.out.println((GlobalOptions.decimalformat.format((double)step/ (double) total * 100)) + " %...");
 		System.gc();
 		System.out.println((Runtime.getRuntime().totalMemory()- Runtime.getRuntime().freeMemory())/(1024*1024) + " MB ");
