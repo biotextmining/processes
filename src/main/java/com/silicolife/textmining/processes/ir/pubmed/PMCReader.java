ea52e2f14ec14e90995e5486c7f7519be23f8732
Ruben Rodrigues <rrodrigues@silicolife.com>
2016-08-29 16:13:40 +0100

    [Update] PMC reader to get the full text

--- src/main/java/com/silicolife/textmining/processes/ir/pubmed/PMCReader.java
+++ src/main/java/com/silicolife/textmining/processes/ir/pubmed/PMCReader.java
@@ -3,21 +3,25 @@ package com.silicolife.textmining.processes.ir.pubmed;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import com.silicolife.textmining.core.datastructures.dataaccess.database.dataaccess.implementation.utils.PublicationFieldTypeEnum;
 import com.silicolife.textmining.core.datastructures.documents.PublicationExternalSourceLinkImpl;
 import com.silicolife.textmining.core.datastructures.documents.PublicationImpl;
 import com.silicolife.textmining.core.datastructures.documents.PublicationSourcesDefaultEnum;
+import com.silicolife.textmining.core.datastructures.documents.structure.PublicationFieldImpl;
+import com.silicolife.textmining.core.datastructures.textprocessing.NormalizationForm;
 import com.silicolife.textmining.core.interfaces.core.dataaccess.exception.ANoteException;
 import com.silicolife.textmining.core.interfaces.core.document.IPublication;
 import com.silicolife.textmining.core.interfaces.core.document.IPublicationExternalSourceLink;
@@ -25,15 +29,16 @@ import com.silicolife.textmining.core.interfaces.core.document.labels.IPublicati
 import com.silicolife.textmining.core.interfaces.core.document.structure.IPublicationField;
 
 public class PMCReader {
-	
+
+	private List<IPublication> publications ;
 	public static final String pubmedLink = "http://www.ncbi.nlm.nih.gov/pubmed/";
 
 	public PMCReader(){
-		
+		this.publications = new ArrayList<>();
 	}
 
-	
-	public IPublication getPublications(File xxmlFile) throws ANoteException{
+
+	public List<IPublication> getPublications(File xxmlFile) throws ANoteException{
 		try{
 			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 			dbf.setValidating(false);
@@ -43,67 +48,79 @@ public class PMCReader {
 			dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-dtd-grammar", false);
 			dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
 			Document doc = dbf.newDocumentBuilder().parse(xxmlFile);
-			
-//			XPathFactory factory = XPathFactory.newInstance();
-//			XPath xpath = factory.newXPath();
-			NodeList nodesArticle = doc.getElementsByTagName("article-meta");
-			Element elements = (Element) nodesArticle.item(0);
-			
-//			Element pubElements = (Element) nodesPubMed.item(j);
-//			NodeList node = elements.getElementsByTagName("PMID");
-//			String pubmedID = node.item(0).getTextContent();
-			List<IPublicationExternalSourceLink> externalIDsSource = processExternalIds(elements);
+			NodeList nodes = doc.getElementsByTagName("article");
 
-			String title = "";//processArticleTitle(elements);
-			//				String authorList = "";
-			String authorList = "" ;// processAuthorList(nodes, lastNameExpresion, j, elements);
+			for (int j = 0; j < nodes.getLength(); j++) {
+				Element elements = (Element) nodes.item(j);
 
-			List<IPublicationField> fullTextfields = new ArrayList<IPublicationField>();
-			String abstractText = ""; //processAbstract(fullTextfields, elements);
-			String type = "" ; //processSimpleElementTagName(elements, "PublicationType");
+				List<IPublicationExternalSourceLink> externalIDsSource = processExternalIds(elements);
 
-			List<IPublicationLabel> labels = new ArrayList<IPublicationLabel>();
+				String title = processArticleTitle(elements);
+				String authorList = processAuthorList(elements);
 
-			String status = ""; //processSimpleElementTagName(elements, "PublicationStatus");
-			if(status.length()>25){
-				status = status.substring(0,24);
+				List<IPublicationField> fullTextfields = new ArrayList<IPublicationField>();
+				String abstractText = processAbstract(fullTextfields, elements);
+				String type = "" ; //processSimpleElementTagName(elements, "PublicationType");
+
+				List<IPublicationLabel> labels = new ArrayList<IPublicationLabel>();
+
+				String status = ""; //processSimpleElementTagName(elements, "PublicationStatus");
+				if(status.length()>25){
+					status = status.substring(0,24);
+				}
+				String journal = processSimpleElementTagName(elements,"journal-title-group").trim();
+				String pages = processSimpleElementTagName(elements, "fpage") + "-" + processSimpleElementTagName(elements, "lpage");
+				if(pages.length()>128){
+					System.out.println("Pages more that 128 characteres ");
+					System.out.println("Pages : " + pages);
+					pages = pages.substring(0,127);
+				}
+				String volume = processSimpleElementTagName(elements, "volume");
+				if(volume.length()>128){
+					System.out.println("Volume more that 128 characteres ");
+					System.out.println("Volume : "+volume);
+					volume = volume.substring(0, 127);
+				}
+				String issues = processSimpleElementTagName(elements, "issue");
+				if(issues.length()>128){
+					System.out.println("Issue more that 128 characteres ");
+					System.out.println("Issue : "+issues);
+					issues = issues.substring(0, 127);
+				}
+				String date = processPublicationDate(elements);
+				if(date.length()>25){
+					System.out.println("Date more that 25 characteres ");
+					System.out.println("Date : "+date);
+					date = date.substring(0, 24);
+				}
+
+
+
+				String yearDate = new String();
+				if(date.length()>3)
+					yearDate = date.substring(0,4);
+
+				String fullTextContent = processFullText(fullTextfields, elements);
+
+				IPublication publication = new PublicationImpl(title, authorList, type, yearDate, date, status, journal, volume,
+						issues, pages, abstractText, "", true, new String(), new String(), externalIDsSource, fullTextfields , labels );
+				publication.setFullTextContent(fullTextContent);
+				addPublication(publication);
 			}
-			String journal = "" ;//processSimpleElementTagName(elements,"Title");
-			String pages = ""; //processSimpleElementTagName(elements, "MedlinePgn");
-			if(pages.length()>128){
-				System.out.println("Pages more that 128 characteres ");
-				System.out.println("Pages : " + pages);
-				pages = pages.substring(0,127);
-			}
-			String volume = ""; //processSimpleElementTagName(elements, "Volume");
-			if(volume.length()>128){
-				System.out.println("Volume more that 128 characteres ");
-				System.out.println("Volume : "+volume);
-				volume = volume.substring(0, 127);
-			}
-			String issues = ""; //processSimpleElementTagName(elements, "Issue");
-			if(issues.length()>128){
-				System.out.println("Issue more that 128 characteres ");
-				System.out.println("Issue : "+issues);
-				issues = issues.substring(0, 127);
-			}
-			String date = ""; //processPublicationDate(elements);
-			if(date.length()>25){
-				System.out.println("Date more that 25 characteres ");
-				System.out.println("Date : "+date);
-				date = date.substring(0, 24);
-			}
 
-			String yearDate = new String();
-			if(date.length()>3)
-				yearDate = date.substring(0,4);
-			return new PublicationImpl(title, authorList, type, yearDate, date, status, journal, volume,
-					issues, pages, abstractText, "", false, new String(), new String(), externalIDsSource, fullTextfields , labels );
-
+			return getPublications();
 		}catch(SAXException | IOException | ParserConfigurationException e){
 			throw new ANoteException(e);
 		}
 
+	}
+
+	public List<IPublication> getPublications() {
+		return publications;
+	}
+
+	public void addPublication(IPublication pub){
+		getPublications().add(pub);
 	}
 
 
@@ -116,21 +133,192 @@ public class PMCReader {
 			String source = extenalIDElem.getAttribute("pub-id-type");
 			String externalID = extenalIDElem.getTextContent();
 			switch (source) {
-				case "pmid" :
-					externalIdsList.add(new PublicationExternalSourceLinkImpl(externalID, PublicationSourcesDefaultEnum.PUBMED.toString()));
-					break;
-				case "pmc" :
-					externalIdsList.add(new PublicationExternalSourceLinkImpl(externalID, PublicationSourcesDefaultEnum.pmc.toString()));
-					break;
-				case "doi" :
-					externalIdsList.add(new PublicationExternalSourceLinkImpl(externalID, PublicationSourcesDefaultEnum.DOI.toString()));
-					break;
-				default :
-					break;
+			case "pmid" :
+				externalIdsList.add(new PublicationExternalSourceLinkImpl(externalID, PublicationSourcesDefaultEnum.PUBMED.toString()));
+				break;
+			case "pmc" :
+				externalIdsList.add(new PublicationExternalSourceLinkImpl(externalID, PublicationSourcesDefaultEnum.pmc.toString()));
+				break;
+			case "doi" :
+				externalIdsList.add(new PublicationExternalSourceLinkImpl(externalID, PublicationSourcesDefaultEnum.DOI.toString()));
+				break;
+			default :
+				break;
 			}
 		}
 		return externalIdsList;
 	}
 
+	private String processArticleTitle(Element elements) {
+		String title;
+		title = processSimpleElementTagName(elements, "title-group");
+		title = NormalizationForm.removeOffsetProblemSituation(title);
+		return title;
+	}
+
+	private String processAuthorList(Element elements){
+		String authors = new String();
+		NodeList contributions = elements.getElementsByTagName("contrib-group");
+		for(int i=0;i<contributions.getLength();i++){
+			Element contribution = (Element) contributions.item(i);
+			if(contribution != null){
+				NodeList contribs = contribution.getElementsByTagName("contrib");
+				for(int ii=0;ii<contribs.getLength();ii++){
+					Element contrib = (Element) contribs.item(ii);
+					if(contrib != null){
+						String att = contrib.getAttribute("contrib-type");
+						if(att != null && att.equals("author")){
+							NodeList names = contrib.getElementsByTagName("name");
+							for(int j=0;j<names.getLength();j++){
+								Element name = (Element) names.item(j);
+								if(!authors.isEmpty()){
+									authors = authors + ", ";
+								}
+								authors = authors + processSimpleElementTagName(name, "given-names") + " ";
+								authors = authors + processSimpleElementTagName(name, "surname");
+							}
+						}
+					}
+				}
+			}
+
+		}
+		return authors;
+	}
+
+	private String processSimpleElementTagName(Element elements, String tagName) {
+		String result = new String();
+		NodeList node = elements.getElementsByTagName(tagName);
+		if (node.item(0) != null) {
+			result = node.item(0).getTextContent();
+		}
+		return result;
+	}
+
+	public String processAbstract(List<IPublicationField> fullTextfields,
+			Element elements) throws ANoteException{
+		NodeList node = elements.getElementsByTagName("abstract");
+		String abstractText = new String();
+		Set<String> publicationFiledsAlreayAdded = new HashSet<>();
+		for(int i=0;i<node.getLength();i++)
+		{
+			if (node.item(i) != null) {
+				int startindex = abstractText.length();
+				String abstractParagraph = node.item(i).getTextContent();
+				abstractParagraph = NormalizationForm.removeOffsetProblemSituation(abstractParagraph);
+				if(abstractText.isEmpty() || Character.isWhitespace(abstractText.charAt(abstractText.length()-1))){
+					abstractText = abstractText + abstractParagraph;
+				}else{
+					abstractText = abstractText +" "+ abstractParagraph;
+				}
+				int endindex = abstractText.length();
+				if(node.item(i).getAttributes().getNamedItem("Label")!=null)
+				{
+					String field = node.item(i).getAttributes().getNamedItem("Label").getTextContent();
+					if(!field.isEmpty() && !publicationFiledsAlreayAdded.contains(field))
+					{
+						if(field.length()>250)
+							field = field.substring(0,250);
+						IPublicationField publicationField = new PublicationFieldImpl(startindex, endindex, field, PublicationFieldTypeEnum.abstracttext);
+						fullTextfields.add(publicationField);
+						publicationFiledsAlreayAdded.add(field);
+					}
+				}
+			}
+		}
+		return abstractText;
+	}
+
+	private String processPublicationDate(Element elements) {
+		NodeList node = elements.getElementsByTagName("pub-date");
+		String date = new String();
+		if (node.item(0) != null) {
+			NodeList childs = node.item(0).getChildNodes();
+			for (int i = 0; i < childs.getLength(); i++) {
+				if (childs.item(i).getTextContent().matches("[a-zA-Z0-9]+")) {
+					date += childs.item(i).getTextContent() + "-";
+				}
+			}
+			if (date.endsWith("-")) {
+				date = date.substring(0, date.length() - 1);
+			}
+		}
+		return date;
+	}
+
+	public String processFullText(List<IPublicationField> fullTextfields,
+			Element elements) throws ANoteException{
+		NodeList node = elements.getElementsByTagName("body");
+		String fullText = new String();
+		Set<String> publicationFiledsAlreayAdded = new HashSet<>();
+		for(int i=0;i<node.getLength();i++)
+		{
+			if (node.item(i) != null) {
+				Element body = (Element) node.item(i);
+				List<Element> bodyElements = getAllBodyElements(body);
+				
+				int startindex = fullText.length();
+				String field = new String();
+				for(Element bodypart : bodyElements){
+					String textParagraph = bodypart.getTextContent();
+					textParagraph = NormalizationForm.removeOffsetProblemSituation(textParagraph);
+					if(fullText.isEmpty() || Character.isWhitespace(fullText.charAt(fullText.length()-1))){
+						fullText = fullText + textParagraph;
+					}else{
+						fullText = fullText +" "+ textParagraph;
+					}
+					if(bodypart.getTagName().equals("title"))
+					{
+						int endindex = fullText.length() - bodypart.getTextContent().length();
+						if(!field.isEmpty() && !publicationFiledsAlreayAdded.contains(field) && startindex != endindex)
+						{
+							if(field.length()>250)
+								field = field.substring(0,250);
+							IPublicationField publicationField = new PublicationFieldImpl(startindex, endindex, field, PublicationFieldTypeEnum.fulltext);
+							fullTextfields.add(publicationField);
+							publicationFiledsAlreayAdded.add(field);
+							startindex = endindex;
+						}
+						field = bodypart.getTextContent();
+					}
+				}
+				if(startindex != fullText.length()){
+					int endindex = fullText.length();
+					if(!field.isEmpty() && !publicationFiledsAlreayAdded.contains(field) && startindex != endindex)
+					{
+						if(field.length()>250)
+							field = field.substring(0,250);
+						IPublicationField publicationField = new PublicationFieldImpl(startindex, endindex, field, PublicationFieldTypeEnum.fulltext);
+						fullTextfields.add(publicationField);
+						publicationFiledsAlreayAdded.add(field);
+						startindex=fullText.length();
+					}
+				}
+				
+			}
+
+		}
+		return fullText;
+	}
+
+	private List<Element> getAllBodyElements(Element body){
+		List<Element> partsBody = new ArrayList<>();
+		fillBodyParts(body, partsBody);
+		return partsBody;
+	}
+
+	private void fillBodyParts(Element part, List<Element> bodyParts){
+		if(part.getChildNodes() != null && part.getChildNodes().getLength()!=1 && !part.getTagName().equals("p")){
+			NodeList childs = part.getChildNodes();
+			for(int i = 0; i<childs.getLength(); i++){
+				Node child = childs.item(i);
+				if(child instanceof Element){
+					fillBodyParts((Element)child, bodyParts);
+				}
+			}
+		}else{
+			bodyParts.add(part);
+		}
+	}
 
 }
