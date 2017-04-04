aa9bb39825d5a734ba102e59796fd3a494a1f811
Hugo Costa <hcosta@silicolife.com>
2017-04-04 15:07:58 +0100

    [bug fix] Drugbank loader

--- src/main/java/com/silicolife/textmining/processes/resources/dictionary/loaders/drugbank/DrugBankOpenDataCSVLoader.java
+++ src/main/java/com/silicolife/textmining/processes/resources/dictionary/loaders/drugbank/DrugBankOpenDataCSVLoader.java
@@ -9,10 +9,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
-import com.silicolife.textmining.core.datastructures.general.AnoteClass;
 import com.silicolife.textmining.core.datastructures.general.ExternalIDImpl;
 import com.silicolife.textmining.core.datastructures.general.SourceImpl;
-import com.silicolife.textmining.core.datastructures.resources.ResourceElementImpl;
 import com.silicolife.textmining.core.datastructures.resources.dictionary.loaders.DictionaryLoaderHelp;
 import com.silicolife.textmining.core.datastructures.resources.dictionary.loaders.configuration.DictionaryLoaderConfigurationImpl;
 import com.silicolife.textmining.core.datastructures.utils.FileHandling;
@@ -26,7 +24,6 @@ import com.silicolife.textmining.core.interfaces.core.report.resources.IResource
 import com.silicolife.textmining.core.interfaces.process.IE.io.export.DefaultDelimiterValue;
 import com.silicolife.textmining.core.interfaces.process.IE.io.export.Delimiter;
 import com.silicolife.textmining.core.interfaces.process.IE.io.export.TextDelimiter;
-import com.silicolife.textmining.core.interfaces.resource.IResourceElement;
 import com.silicolife.textmining.core.interfaces.resource.dictionary.IDicionaryFlatFilesLoader;
 import com.silicolife.textmining.core.interfaces.resource.dictionary.configuration.IDictionaryLoaderConfiguration;
 
@@ -122,7 +119,7 @@ public class DrugBankOpenDataCSVLoader extends DictionaryLoaderHelp implements I
 			step++;
 			if(step%100==0)
 			{
-				memoryAndProgress(starttime,  step, total);
+				memoryAndProgress(step, total);
 			}
 		}
 		if (!cancel) {
@@ -137,9 +134,8 @@ public class DrugBankOpenDataCSVLoader extends DictionaryLoaderHelp implements I
 	}
 
 	private void processDrugLine(String line, boolean importExternlIDs) throws ANoteException {
-		System.out.println("###Drug");
-
 		List<IExternalID> externalIDs = new ArrayList<>();
+		int correctionfactor = 0;
 		String[] lineSplitered = line.split(csvConfigurations.getGeneralDelimiter().getValue());
 		String dbprimaryID = lineSplitered[csvConfigurations.getColumsDelemiterDefaultValue().getColumnNameColumnParameters().get(columnDrugBAnkIDName).getColumnNumber()];
 		externalIDs.add(new ExternalIDImpl(dbprimaryID.trim(), new SourceImpl(drugbanksource)));	
@@ -148,13 +144,33 @@ public class DrugBankOpenDataCSVLoader extends DictionaryLoaderHelp implements I
 		for(String alternativeDrugBankId:alternativeDrugBankIds)
 			externalIDs.add(new ExternalIDImpl(alternativeDrugBankId.trim(), new SourceImpl(drugbanksource)));	
 		String primartyName = lineSplitered[csvConfigurations.getColumsDelemiterDefaultValue().getColumnNameColumnParameters().get(columnCommonNameName).getColumnNumber()];
-		String cas = lineSplitered[csvConfigurations.getColumsDelemiterDefaultValue().getColumnNameColumnParameters().get(columnCommonCASName).getColumnNumber()];
-		externalIDs.add(new ExternalIDImpl(cas.trim(), new SourceImpl(CASsource)));	
+		boolean cancel = false;
+		if(primartyName.trim().startsWith("\""))
+		{
+			for(int i=csvConfigurations.getColumsDelemiterDefaultValue().getColumnNameColumnParameters().get(columnCommonNameName).getColumnNumber()+1
+					;i<lineSplitered.length && !cancel;i++)
+			{
+				if(lineSplitered[i].endsWith("\""))
+				{
+					cancel = true;
+				}
+				primartyName = primartyName + lineSplitered[i];
+				correctionfactor++;
+			}
+			primartyName = primartyName.replace("\"", "");
+		}
+		String cas = lineSplitered[csvConfigurations.getColumsDelemiterDefaultValue().getColumnNameColumnParameters().get(columnCommonCASName).getColumnNumber()+correctionfactor];
+		if(!cas.trim().isEmpty())
+			externalIDs.add(new ExternalIDImpl(cas.trim(), new SourceImpl(CASsource)));	
 		if(!line.endsWith(","))
 		{
 			externalIDs.add(new ExternalIDImpl(lineSplitered[lineSplitered.length-1].trim(), new SourceImpl(InCHIsource)));	
 		}
-		List<String> synonyms =  getSynonyms(line, lineSplitered);
+		List<String> synonyms =  getSynonyms(line, lineSplitered,correctionfactor);
+//		System.out.println("#### Drug");
+//		System.out.println(primartyName.trim());
+//		System.out.println(synonyms);
+//		System.out.println(externalIDs);
 		super.addElementToBatch(primartyName.trim(), drug, new HashSet<>(synonyms), externalIDs, 0);
 		if (!cancel && isBatchSizeLimitOvertaken()) {
 			IResourceManagerReport reportBatchInserted = super.executeBatch();
@@ -162,7 +178,7 @@ public class DrugBankOpenDataCSVLoader extends DictionaryLoaderHelp implements I
 		}
 	}
 	
-	private List<String> getSynonyms(String line,String[] lineSplitered)
+	private List<String> getSynonyms(String line,String[] lineSplitered, int correctionfactor)
 	{
 		int finalNumber = lineSplitered.length;
 		if(!line.endsWith(","))
@@ -170,7 +186,7 @@ public class DrugBankOpenDataCSVLoader extends DictionaryLoaderHelp implements I
 			finalNumber = lineSplitered.length-1;
 		}
 		String strConcat = new String();
-		for(int i = 5;i< finalNumber;i++)
+		for(int i = 5+correctionfactor;i< finalNumber;i++)
 		{
 			strConcat = strConcat + lineSplitered[i];
 		}
@@ -223,7 +239,32 @@ public class DrugBankOpenDataCSVLoader extends DictionaryLoaderHelp implements I
 		return testFileHeader(file);
 	}
 	
-	private boolean testFileHeader(File file)
+
+	public static boolean checkFileDrugBank(File file) {
+		String dialogMsg = "";
+		String consoleMsg = "";
+		if (!file.getName().endsWith(".csv")) {
+            dialogMsg = consoleMsg = "Invalid file format: CSV required!";
+        }
+		else if(!file.exists()){
+			dialogMsg = consoleMsg = "Selected file does not exist!";
+		}
+		else if(!file.isFile()){
+			dialogMsg = consoleMsg = "You haven't select a file!";
+		}
+		else if(!file.canRead()){
+			dialogMsg = consoleMsg = "Selected file cannot be read!";
+		}
+		else if(file.length() <= 0){
+			dialogMsg = consoleMsg = "Selected file is empty";
+		}
+		if(!dialogMsg.equals("") || !consoleMsg.equals("")){
+			return false;
+		}
+		return testFileHeader(file);
+	}
+	
+	private static boolean testFileHeader(File file)
 	{
 		try {
 			String header = FileHandling.getFileFirstLine(file);
