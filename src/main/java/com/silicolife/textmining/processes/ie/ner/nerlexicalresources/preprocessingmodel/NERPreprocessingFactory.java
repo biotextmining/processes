501ef0b6f7fd22778e347be1aa214cdbf66d1767
RRodrigues <Ru13en@users.noreply.github.com>
2017-05-03 11:10:52 +0100

    [BugFix] Native NER null pointer elements

--- src/main/java/com/silicolife/textmining/processes/ie/ner/nerlexicalresources/preprocessingmodel/NERPreprocessingFactory.java
+++ src/main/java/com/silicolife/textmining/processes/ie/ner/nerlexicalresources/preprocessingmodel/NERPreprocessingFactory.java
@@ -2,31 +2,34 @@ package com.silicolife.textmining.processes.ie.ner.nerlexicalresources.preproces
 
 import java.util.List;
 
+import com.silicolife.textmining.core.datastructures.process.ner.ElementToNer;
 import com.silicolife.textmining.core.interfaces.core.annotation.IEntityAnnotation;
+import com.silicolife.textmining.core.interfaces.core.dataaccess.exception.ANoteException;
 import com.silicolife.textmining.processes.ie.ner.nerlexicalresources.configuration.INERLexicalResourcesConfiguration;
 import com.silicolife.textmining.processes.ie.ner.nerlexicalresources.configuration.INERLexicalResourcesPreProcessingModel;
 import com.silicolife.textmining.processes.ie.ner.nerlexicalresources.configuration.NERLexicalResourcesPreProssecingEnum;
 
 public class NERPreprocessingFactory {
 
-	public static INERLexicalResourcesPreProcessingModel build(INERLexicalResourcesConfiguration lexicalResurcesConfiguration, NERLexicalResourcesPreProssecingEnum preprocessing) {
+	public static INERLexicalResourcesPreProcessingModel build(INERLexicalResourcesConfiguration lexicalResurcesConfiguration, NERLexicalResourcesPreProssecingEnum preprocessing) throws ANoteException {
 		
-		List<IEntityAnnotation> termAnnotations = null;
+		ElementToNer elemntsToNER = new ElementToNer(lexicalResurcesConfiguration.getResourceToNER(), lexicalResurcesConfiguration.isNormalized());
+		List<IEntityAnnotation> elements = elemntsToNER.getTerms();
 		if(preprocessing.equals(NERLexicalResourcesPreProssecingEnum.No))
 		{
-			return new NERSimple(termAnnotations);
+			return new NERSimple(elements);
 		}
 		else if(preprocessing.equals(NERLexicalResourcesPreProssecingEnum.StopWords))
 		{
-			return new NERPreProcessingStopWords(termAnnotations, lexicalResurcesConfiguration.getStopWords());
+			return new NERPreProcessingStopWords(elements, lexicalResurcesConfiguration.getStopWords());
 		}
 		else if(preprocessing.equals(NERLexicalResourcesPreProssecingEnum.POSTagging))
 		{
-			return new NERPreProcessingPOSTagging(termAnnotations, lexicalResurcesConfiguration.getPOSTags());
+			return new NERPreProcessingPOSTagging(elements, lexicalResurcesConfiguration.getPOSTags());
 		}
 		else if(preprocessing.equals(NERLexicalResourcesPreProssecingEnum.Hybrid))
 		{
-			return new NERPreProcessingPOSTaggingAndStopWords(termAnnotations, lexicalResurcesConfiguration.getPOSTags(), lexicalResurcesConfiguration.getStopWords());
+			return new NERPreProcessingPOSTaggingAndStopWords(elements, lexicalResurcesConfiguration.getPOSTags(), lexicalResurcesConfiguration.getStopWords());
 		}
 		return null;
 	}
