62bafc0309ad524fd90b714e595f78137b30fb29
Hugo Costa <hcosta@silicolife.com>
2017-07-03 15:46:27 +0100

    [update] ChemSpider API - Get External Ids
    [update] ChemSpider API - Get External Vendors Ids

--- src/main/java/com/silicolife/textmining/processes/resources/dictionary/loaders/chemspider/ChemSpiderAPI.java
+++ src/main/java/com/silicolife/textmining/processes/resources/dictionary/loaders/chemspider/ChemSpiderAPI.java
@@ -9,15 +9,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.XML;
 
+import com.silicolife.textmining.core.datastructures.general.ExternalIDImpl;
+import com.silicolife.textmining.core.datastructures.general.SourceImpl;
 import com.silicolife.textmining.core.datastructures.utils.GenericPairComparable;
 import com.silicolife.textmining.core.interfaces.core.dataaccess.exception.ANoteException;
+import com.silicolife.textmining.core.interfaces.core.general.IExternalID;
 
 public class ChemSpiderAPI {
 
-	static String urlGetCSIDByInchi  = "http://www.chemspider.com/InChI.asmx/InChIToCSID";
+	private static String urlGetCSIDByInchi  = "http://www.chemspider.com/InChI.asmx/InChIToCSID";
+	private static String urlCSID2ExtRefs = "http://www.chemspider.com/Search.asmx/CSID2ExtRefs";
 
 	public static String getCSIDGivenInchi(String accessToken, String inchi) throws ANoteException
 	{
@@ -33,6 +38,47 @@ public class ChemSpiderAPI {
 		} catch (IOException e) {
 			throw new ANoteException(e);
 		}
+	}
+
+	public static List<IExternalID> getExternalIdsGivenCSID(String token,String csid) throws ANoteException
+	{
+		List<IExternalID> out = new ArrayList<>();
+		try {
+			JSONArray jsonArray = getExternalLinks(token,csid,getExternalInterestSources());
+			out.addAll(convertJsonResponseToExternalIdList(jsonArray));
+		} catch (IOException e) {
+			throw new ANoteException(e);
+		}	
+		return out;
+	}
+
+	public static List<IExternalID> getExternalVendorsIdsGivenCSID(String token,String csid) throws ANoteException
+	{
+		List<IExternalID> out = new ArrayList<>();
+		try {
+			JSONArray jsonArray = getExternalLinks(token,csid,getVendors());
+			out.addAll(convertJsonResponseToExternalIdList(jsonArray));
+		} catch (IOException e) {
+			throw new ANoteException(e);
+		}	
+		return out;
+	}
+
+	private static List<IExternalID> convertJsonResponseToExternalIdList(JSONArray jsonArray)
+	{
+		List<IExternalID> out = new ArrayList<>();
+		for(int i=0;i<jsonArray.length();i++)
+		{
+			JSONObject element = (JSONObject) jsonArray.get(i);
+			if(element.has("ds_name")&& element.has("ext_id"))
+			{
+				String source = element.get("ds_name").toString();
+				String externalID = element.get("ext_id").toString();
+				IExternalID ext = new ExternalIDImpl(externalID, new SourceImpl(source));
+				out.add(ext);
+			}
+		}
+		return out;
 	}
 
 	private static String apiCall(String endpoint, List<GenericPairComparable<String, String>> data) throws IOException {
@@ -61,8 +107,371 @@ public class ChemSpiderAPI {
 	private static List<GenericPairComparable<String, String>> apiInChIToCSIDData(String token,String inchi) {
 		List<GenericPairComparable<String, String>> data = new ArrayList<>();
 		data.add(new GenericPairComparable<String, String>("inchi", inchi));
-	    data.add(new GenericPairComparable<String, String>("token", token));
+		data.add(new GenericPairComparable<String, String>("token", token));
 		return data;
+	}
+
+	private static JSONArray getExternalLinks(String token,String csid, List<String> externalSources) throws IOException {
+		String xml = apiCall(urlCSID2ExtRefs, apiCSID2ExtRefsdata(csid, token,externalSources));
+		JSONObject toJSON = XML.toJSONObject(xml);
+		if(toJSON.getJSONObject("ArrayOfExtRef")!=null)
+		{
+			Object externalIdslist = toJSON.getJSONObject("ArrayOfExtRef").get("ExtRef");
+			JSONArray wrapped = new JSONArray();
+			if (externalIdslist instanceof JSONArray) {
+				wrapped = (JSONArray)externalIdslist;
+			} else if (externalIdslist instanceof JSONObject) {
+				wrapped.put(externalIdslist);
+			} else {
+				throw new IOException(externalIdslist.getClass().getName());
+			}
+			return wrapped;
+		}
+		return new JSONArray();
+	}
+
+	private static List<GenericPairComparable<String, String>> apiCSID2ExtRefsdata(String csid, String token, List<String> externalSources) {
+		List<GenericPairComparable<String, String>> data = new ArrayList<>();
+		data.add(new GenericPairComparable<String, String>("CSID", csid.toString()));
+		data.add(new GenericPairComparable<String, String>("token", token));
+		List<String> datasrcs = new ArrayList<>();
+		datasrcs.addAll(externalSources);
+		for (String datasrc : datasrcs )
+			data.add(new GenericPairComparable<String, String>("datasources", datasrc));
+		return data;
+	}
+
+	private static List<String> getExternalInterestSources()
+	{
+		List<String> exernalinterestSources = new ArrayList<>();
+		exernalinterestSources.add("CHEBI");
+		exernalinterestSources.add("DrugBank");
+		exernalinterestSources.add("CAS");
+		exernalinterestSources.add("BioCyc");
+		exernalinterestSources.add("ChEMBL");
+		exernalinterestSources.add("MeSH");
+		exernalinterestSources.add("PubChem");
+		//		datasrcs.add("Royal Society of Chemistry");
+		exernalinterestSources.add("Wikipedia");
+		exernalinterestSources.add("ZINC");	
+		return exernalinterestSources;
+	}
+
+	private static List<String> getVendors()
+	{
+		String[] datasrcs = getVendorList();
+		List<String> vendorSources = new ArrayList<>();
+		for(String vendor:datasrcs)
+		{
+			vendorSources.add(vendor);
+		}		
+		return vendorSources;
+	}
+
+
+
+	private static String[] getVendorList() {
+		// This data comes from act/reachables/src/main/resources/chemspider-vendors
+		// See the script vendors_from_inchi.sh and the step{1-5} that extract these
+		// sources from ChemSpider. Steps{1-5} result in a file called vendor_names.txt
+		// and that file is pasted here...
+		String[] datasrcs = new String[] {
+				"ASINEX",
+				"ChemBridge",
+				"Specs",
+				"Enamine",
+				"AKos",
+				"R&D Chemicals",
+				"Synthon-Lab",
+				"UkrOrgSynthesis",
+				"CiVentiChem",
+				"SynChem",
+				"Ryan Scientific",
+				"TOSLab",
+				"Bio-Vin",
+				"ChemDiv",
+				"Otava Chemicals",
+				"Aronis",
+				"Life Chemicals",
+				"Calyx",
+				"Activate Scientific",
+				"Argus Chemicals",
+				"AsisChem",
+				"Boron Molecular",
+				"ChemPacific",
+				"Microsource",
+				"Trylead Chemical",
+				"Sigma-Aldrich",
+				"Afid Therapeutics",
+				"Alfa Aesar",
+				"Vitas-M",
+				"Key Organics",
+				"Matrix Scientific",
+				"PepTech",
+				"Pharmeks",
+				"Trans World Chemicals",
+				"Astatech",
+				"Chess Chemical",
+				"JRD Fluorochemicals",
+				"Ubichem",
+				"AnalytiCon Discovery",
+				"MP Biomedicals",
+				"Oakwood",
+				"Exclusive Chemistry",
+				"OmegaChem",
+				"HDH Pharma",
+				"Rieke Metals",
+				"ASDI",
+				"Florida Center for Heterocyclic Compounds",
+				"Synthonix",
+				"Shanghai Sinofluoro Scientific",
+				"Hetcat",
+				"Borochem",
+				"Biosynth",
+				"True PharmaChem",
+				"Cayman Chemical",
+				"Dipharma",
+				"ACB Blocks",
+				"Chemik",
+				"Sequoia Research Products",
+				"Apollo Scientific Limited",
+				"Spectrum Info",
+				"Infarmatik",
+				"Rudolf Boehm Institute",
+				"Timtec",
+				"Tocris Bioscience",
+				"Princeton Biomolecular",
+				"Hangzhou Sage Chemical Co., Ltd.",
+				"Viwit Pharmaceutical",
+				"MicroCombiChem",
+				"SelectLab Chemicals GmbH",
+				"Ramdev Chemicals",
+				"Extrasynthese",
+				"Gelest",
+				"Bridge Organics",
+				"Jiangsu WorldChem",
+				"Baihua Bio-Pharmaceutical",
+				"Szintekon Ltd",
+				"Excel Asia",
+				"Alinda Chemical",
+				"ennopharm",
+				"Manchester Organics",
+				"Globe Chemie",
+				"Shanghai Haoyuan Chemexpress ",
+				"Shanghai Elittes organics",
+				"Cooper Chemicals",
+				"Hangzhou APIChem Technology ",
+				"Mizat Chemicals ",
+				"Frinton Laboratories",
+				"BePharm",
+				"HE Chemical",
+				"Molport",
+				"BioBlocks Inc.",
+				"Zerenex Molecular ",
+				"Innovapharm",
+				"Research Organics",
+				"Creasyn Finechem",
+				"Alchem Pharmtech",
+				"iThemba Pharmaceuticals",
+				"Sun BioChem, Inc.",
+				"Santa Cruz Biotechnology ",
+				"DSL Chemicals",
+				"AvaChem Scientific",
+				"SynQuest",
+				"Evoblocks",
+				"CDN Isotopes",
+				"Endeavour Speciality Chemicals",
+				"Shanghai Race Chemical",
+				"Shanghai IS Chemical Technology",
+				"DanYang HengAn Chemical Co.,Ltd",
+				"ChiroBlock",
+				"Platte Valley Scientific",
+				"TCI",
+				"Finetech Industry",
+				"Nagase",
+				"Annker Organics",
+				"Ark Pharm, Inc.",
+				"Aconpharm",
+				"Endotherm GmbH",
+				"InterBioScreen",
+				"Fluorochem ",
+				"Accela ChemBio",
+				"ChemFuture",
+				"Syntide",
+				"Paragos",
+				"DiverChim",
+				"oriBasePharma",
+				"Chiralix",
+				"AChemo",
+				"Selleck Chemicals",
+				"Watson International Ltd",
+				"Excenen",
+				"Shanghai Boyle Chemical Co., Ltd.",
+				"Alfa Pyridines",
+				"Shanghai Excellent chemical",
+				"Chiral Quest",
+				"AMRI",
+				"Letopharm",
+				"Santai Labs",
+				"Adesis",
+				"AOKChem",
+				"Nanjing Pharmaceutical Factory Co., Ltd",
+				"DAY Biochem",
+				"zealing chem",
+				"ABI Chemicals",
+				"AOKBIO",
+				"Reddy N Reddy Pharmaceuticals",
+				"Chengdu D-innovation",
+				"Avistron Chemistry",
+				"Abacipharm",
+				"Centec",
+				"Focus Synthesis",
+				"Georganics Ltd.",
+				"Rare Chem",
+				"Annova Chem",
+				"Chicago Discovery Solutions",
+				"Solaronix",
+				"Apeiron Synthesis",
+				"Indofine",
+				"J and K Scientific",
+				"Porse Fine Chemical",
+				"Cool Pharm",
+				"Livchem",
+				"Fragmenta",
+				"AEchem Scientific",
+				"Mole-Sci.Tech",
+				"Irvine Chemistry Laboratory ",
+				"Synergy-Scientific",
+				"Angene",
+				"CoachChem",
+				"Abblis Chemicals",
+				"Abcam",
+				"Jalor-Chem",
+				"AK Scientific",
+				"Acorn PharmaTech",
+				"Zylexa Pharma",
+				"Chemren Bio-Engineering",
+				"Isosep",
+				"Selleck Bio",
+				"BOC Chem",
+				"Advanced ChemBlocks",
+				"Juhua Group",
+				"Capot Chemical",
+				"LGC Standards",
+				"Biochempartner",
+				"Adooq Bioscience",
+				"Novochemy",
+				"Atomole Scientific",
+				"Huili Chem",
+				"P3 BioSystems",
+				"Beijing LYS Chemicals",
+				"Hangzhou Chempro",
+				"Abmole Bioscience",
+				"Watec Laboratories",
+				"Apexmol",
+				"Conier Chem",
+				"Amadis Chemical",
+				"Alfa Chemistry",
+				"ADVAMACS",
+				"Jupiter Sciences",
+				"Arking Pharma",
+				"Wisdom Pharma",
+				"KaironKem",
+				"Alchemist-Pharm",
+				"Natural Remedies",
+				"LeadGen Labs",
+				"Acentex Scientific",
+				"Anward",
+				"Rosewell Industry Co.",
+				"Chembo Pharma",
+				"Achemica",
+				"EDASA Scientific",
+				"Sunshine Chemlab",
+				"Acesobio",
+				"Syncozymes",
+				"Chengdu Kaixin",
+				"AminoLogics",
+				"AldLab Chemicals",
+				"ChangChem",
+				"ApexBio",
+				"BerrChem",
+				"Medchem Express",
+				"Merck Millipore",
+				"ChemScene",
+				"Glentham Life Sciences",
+				"Viva Corporation",
+				"PhyStandard",
+				"King Scientific",
+				"eNovation Chemicals",
+				"Thoreauchem",
+				"MolMall",
+				"ACINTS",
+				"Chemodex",
+				"Labseeker",
+				"Axon Medchem",
+				"BroadPharm",
+				"Rosewachem",
+				"Renaissance Chemicals",
+				"CEG Chemical",
+				"GFS Chemicals",
+				"OXchem",
+				"ACT Chemical",
+				"Bide Pharmatech",
+				"Arromax",
+				"Sinova",
+				"Atomax",
+				"TOKU-E",
+				"Mcule",
+				"Active Biopharma",
+				"Finornic Chemicals",
+				"Apollo Scientific Adopted",
+				"LKT Labs",
+				"Carbosynth",
+				"ChemStep",
+				"Wecoochem",
+				"Aromalake",
+				"W&J PharmaChem, Inc.",
+				"Leverton-Clarke",
+				"Airedale Chemical",
+				"Corvinus Chemicals",
+				"Akerr Pharma",
+				"Debyesci",
+				"Xinyanhe Pharmatech",
+				"Megazyme International",
+				"Arkema",
+				"Advanced Technology & Industrial",
+				"Shenzhen Nexconn Pharmatechs Ltd.",
+				"Aspira Scientific",
+				"Shanghai Pengteng Fine Chemical Co., Ltd. ",
+				"Wylton Jinglin",
+				"AZEPINE",
+				"Attomarker",
+				"OlainFarm",
+				"TripleBond",
+				"Exim",
+				"Helix Molecules",
+				"Santiago Laboratory Equipment",
+				"ChiralStar",
+				"Wolves Chemical",
+				"Hello Bio",
+				"SLI Technologies",
+				"A1 BioChem Labs",
+				"Tubepharm",
+				"A&J Pharmtech",
+				"Aoyi International",
+				"4C Pharma Scientific",
+				"ACO Pharm",
+				"Chemcia Scientific",
+				"Natural Products Discovery Institute",
+				"A2Z Chemical",
+				"GuiChem",
+				"Acemol",
+				"Boerchem",
+				"Suntto Chemical",
+				"SynInnova",
+				"Founder Pharma"
+		};
+		return datasrcs;
 	}
 
 }
--- src/test/java/com/silicolife/textmining/processes/resources/dictionaries/loaders/chemspider/ChemSpiderAPITest.java
+++ src/test/java/com/silicolife/textmining/processes/resources/dictionaries/loaders/chemspider/ChemSpiderAPITest.java
@@ -1,17 +1,36 @@
 package com.silicolife.textmining.processes.resources.dictionaries.loaders.chemspider;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import com.silicolife.textmining.core.interfaces.core.dataaccess.exception.ANoteException;
+import com.silicolife.textmining.core.interfaces.core.general.IExternalID;
 import com.silicolife.textmining.processes.resources.dictionary.loaders.chemspider.ChemSpiderAPI;
 
 public class ChemSpiderAPITest {
 
-	@Test
-	public void test() throws ANoteException {
+//	@Test
+	public void getCSIDGivenInchi() throws ANoteException {
 		String inchi = "InChI=1S/C8H8O3/c1-11-8-4-6(5-9)2-3-7(8)10/h2-5,10H,1H3";
 		String accessToken = "b3d12bfe-1bcd-4960-a30f-ba876fe7a0fb";
 		String result = ChemSpiderAPI.getCSIDGivenInchi(accessToken, inchi);
+		System.out.println(result);
+	}
+	
+//	@Test
+	public void getExternalIdsGivenCSID() throws ANoteException {
+		String csid = "13860434";
+		String accessToken = "b3d12bfe-1bcd-4960-a30f-ba876fe7a0fb";
+		List<IExternalID> result = ChemSpiderAPI.getExternalIdsGivenCSID(accessToken, csid);
+		System.out.println(result);
+	}
+	
+	@Test
+	public void getExternalVendorsIdsGivenCSID() throws ANoteException {
+		String csid = "13860434";
+		String accessToken = "b3d12bfe-1bcd-4960-a30f-ba876fe7a0fb";
+		List<IExternalID> result = ChemSpiderAPI.getExternalVendorsIdsGivenCSID(accessToken, csid);
 		System.out.println(result);
 	}
 
